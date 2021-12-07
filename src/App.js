5e99b77a502b790a8906e107737bbfa2a718e3d6
codingSlate <ananta2021k@gmail.com>
2021-12-07 10:54:02 +0530

    Array in string interpolation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Breakfast from './Breakfast';
 import './style.css';
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>Array in React</h1>
+      <Breakfast />
     </div>
   );
 }
---
+++ src/Breakfast.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Breakfast() {
+  const food = ['q', 'e', 'r', 't'];
+  return (
+    <div>
+      <p>Array of food {food}</p>
+    </div>
+  );
+}
