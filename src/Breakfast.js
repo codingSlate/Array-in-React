d2582693a331eb383d6af765349f1b9f7a9c3f94
codingSlate <ananta2021k@gmail.com>
2021-12-07 13:07:06 +0530

    1 file modified

--- src/Breakfast.js
+++ src/Breakfast.js
@@ -1,10 +1,27 @@
 import React from 'react';
 
 export default function Breakfast() {
-  const food = ['q', 'e', 'r', 't'];
+  const breakfast = ['Rice', 'Puri', 'Dosa', 'Idly', 'Parotha'];
+
+  const breakfast1 = ['Rice', 'Puri', 'Dosa', 'Idly', 'Parotha'];
+  const breakfast2 = ['Rice', 'Puri', 'Dosa', 'Idly', 'Parotha'];
+
+  const DisplayList = breakfast1.map((item, index) => {
+    <li key={index.toString()}>
+      <label htmlFor={`item-${index}`}>{item}</label>
+    </li>;
+  });
   return (
     <div>
-      <p>Array of food {food}</p>
+      <p>Array of food {breakfast}</p>
+      <p>Map</p>
+      <ul>
+        {breakfast.map((f) => (
+          <li key={f}>{f}</li>
+        ))}
+      </ul>
+      <hr />
+      <ul>{DisplayList}</ul>
     </div>
   );
 }
